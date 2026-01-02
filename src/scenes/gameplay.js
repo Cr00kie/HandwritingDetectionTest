2b0859ef005090527cb0e027834b1a217a7a66bb
Adrian Isasi <aisasi505@gmail.com>
2026-01-02 15:23:02 +0100

    add .js at the end of imports

--- src/scenes/gameplay.js
+++ src/scenes/gameplay.js
@@ -1,5 +1,5 @@
-import Phaser from "phaser";
-import { StrokeParser } from "../strokeParser";
+import Phaser from "phaser.js";
+import { StrokeParser } from "../strokeParser.js";
 
 export class Gameplay extends Phaser.Scene{
     constructor(){
